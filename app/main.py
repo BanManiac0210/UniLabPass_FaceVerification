4aeef38792f12d8eb62416b220306acf58e5393d
BanManiac0210 <131276571+BanManiac0210@users.noreply.github.com>
2025-03-19 06:43:56 +0700

    check port

--- app/main.py
+++ app/main.py
@@ -11,5 +11,5 @@ app.include_router(router)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # Mặc định là 8000 nếu không có PORT
-    print(" Server chạy trên cổng {port}")
+    print(f"Server chạy trên cổng {port}")
     uvicorn.run(app, host="0.0.0.0", port=port)
