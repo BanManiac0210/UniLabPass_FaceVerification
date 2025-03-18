f87e21f7ac50c91f98678d30de4cd028a3f13730
BanManiac0210 <131276571+BanManiac0210@users.noreply.github.com>
2025-03-19 05:40:38 +0700

    Fix PORT binding issue

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.endpoints import router
+import uvicorn
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
@@ -9,5 +10,5 @@ app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))  # Lấy PORT từ biến môi trường của Render
+    uvicorn.run(app, host="0.0.0.0", port=port)
