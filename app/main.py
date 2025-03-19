6570541b3ec60a753c749785085f570b9ea16e26
BanManiac0210 <131276571+BanManiac0210@users.noreply.github.com>
2025-03-19 13:13:28 +0700

    update memory usage

--- app/main.py
+++ app/main.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI
 from app.api.endpoints import router
 import uvicorn
 import os
+import tensorflow as tf
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+# Giới hạn lượng RAM TensorFlow sử dụng
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+    tf.config.experimental.set_virtual_device_configuration(
+        gpu, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=256)]
+    )
 
 app = FastAPI()
 
